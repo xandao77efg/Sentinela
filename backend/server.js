90c210307ceae97c076af6ea06f9038d36370266
Carlos Alexandre da Silva Fontenele <carlosalexandre.fontenele@aluno.educa.go.gov.br>
2026-08-21 09:53:12 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -145,7 +145,8 @@ app.get("/medicacoes", (req, res) => {
   res.json(db.consultas);
 });
 
-// START
-app.listen(3000, () => {
-  console.log("🏥 Hospital Pro rodando em http://localhost:3000");
+// start
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Porta ${PORT}`);
 });
